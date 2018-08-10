aebd94c9ae9b60aab30abb1de05a3dbe2a204af4
pmarcinkiewicz1 <39917193+pmarcinkiewicz1@users.noreply.github.com>
2018-08-10 10:35:10 +0200

    Fix unsupported branches. (#258)

--- vars/isBranchUnsupported.groovy
+++ vars/isBranchUnsupported.groovy
@@ -1,15 +1,17 @@
-def SUPPORTED_BRANCHES = [
-    "master"
-]
-
 def wasTriggeredByZuul() {
     return env.ZUUL_UUID != null
 }
 
-def zuulBranchIsUnsupported() {
-    return !SUPPORTED_BRANCHES.contains(env.ZUUL_BRANCH)
+def zuulBranchIsUnsupported(ArrayList<String> supportedBranches) {
+    return !supportedBranches.contains(env.ZUUL_BRANCH)
 }
 
 def call() {
-    return wasTriggeredByZuul() && zuulBranchIsUnsupported()
+	def SUPPORTED_BRANCHES = [
+		"master"
+	]
+
+	echo "Zuulv3: SUPPORTED_BRANCHES = " + SUPPORTED_BRANCHES
+
+    return wasTriggeredByZuul() && zuulBranchIsUnsupported(SUPPORTED_BRANCHES)
 }
